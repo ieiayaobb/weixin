6295902a04916c33e904352dc6ae9d7db8d59110
ieiayaobb <ieiayaobb@gmail.com>
2015-06-24 15:40:23 +0800

    debug arguments

--- Application.py
+++ Application.py
@@ -1,18 +1,26 @@
 __author__ = 'shenyineng'
 
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
-@app.route('/signature?signature=<signature>', methods=['GET'])
-def signature(signature, echostr, timestamp, nonce):
+@app.route('/validate', methods=['GET'])
+def validate():
+    signature = request.args.get('signature')
+    echostr = request.args.get('echostr')
+    timestamp = request.args.get('timestamp')
+    nonce = request.args.get('nonce')
+
     print(signature)
     print(echostr)
     print(timestamp)
     print(nonce)
+
+    return echostr
 
 
 if __name__ == '__main__':
